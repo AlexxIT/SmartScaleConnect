6cdb4e994a94a3c4934c2785f95e5c962e72aa44
Alex X <alexey.khit@gmail.com>
2025-08-07 15:26:58 +0300

    Fix FIT encoder for empty fields

--- pkg/garmin/fit/fit.go
+++ pkg/garmin/fit/fit.go
@@ -22,17 +22,35 @@ func WriteWeight(w io.Writer, weights ...*core.Weight) error {
 		scale.Timestamp = weight.Date
 		scale.Weight = typedef.Weight(weight.Weight * 100)
 
-		scale.Bmi = uint16(weight.BMI * 10)
-		scale.PercentFat = uint16(weight.BodyFat * 100)
-		scale.PercentHydration = uint16(weight.BodyWater * 100)
-		scale.BoneMass = uint16(weight.BoneMass * 100)
+		if weight.BMI != 0 {
+			scale.Bmi = uint16(weight.BMI * 10)
+		}
+		if weight.BodyFat != 0 {
+			scale.PercentFat = uint16(weight.BodyFat * 100)
+		}
+		if weight.BodyWater != 0 {
+			scale.PercentHydration = uint16(weight.BodyWater * 100)
+		}
+		if weight.BoneMass != 0 {
+			scale.BoneMass = uint16(weight.BoneMass * 100)
+		}
 
-		scale.MetabolicAge = uint8(weight.MetabolicAge)
-		scale.MuscleMass = uint16(weight.SkeletalMuscleMass * 100)
-		scale.PhysiqueRating = uint8(weight.PhysiqueRating)
-		scale.VisceralFatRating = uint8(weight.VisceralFat)
+		if weight.MetabolicAge != 0 {
+			scale.MetabolicAge = uint8(weight.MetabolicAge)
+		}
+		if weight.SkeletalMuscleMass != 0 {
+			scale.MuscleMass = uint16(weight.SkeletalMuscleMass * 100)
+		}
+		if weight.PhysiqueRating != 0 {
+			scale.PhysiqueRating = uint8(weight.PhysiqueRating)
+		}
+		if weight.VisceralFat != 0 {
+			scale.VisceralFatRating = uint8(weight.VisceralFat)
+		}
 
-		scale.BasalMet = uint16(weight.BasalMetabolism * 4)
+		if weight.BasalMetabolism != 0 {
+			scale.BasalMet = uint16(weight.BasalMetabolism * 4)
+		}
 
 		//scale.ActiveMet = 0
 		//scale.VisceralFatMass = 0
