8a00d7cde26599494f28d4018ae50aeb77eaefea
Alex X <alexey.khit@gmail.com>
2025-08-12 15:50:28 +0300

    Update run.sh for new cli args

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,4 @@ if [ -f "/data/options.json" ]; then
   SLEEP=$(jq --raw-output ".sleep" /data/options.json)
 fi
 
-while true; do
-  scaleconnect
-  sleep ${SLEEP:-24h}
-done
+scaleconnect -i -r ${SLEEP:-24h}
